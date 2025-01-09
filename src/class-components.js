a50860e5b4120c48c0538d5da155980ad2e36fd3
pat123 <phollakrit@hotmail.com>
2025-01-09 11:25:34 +0700

    add func-components.js

---
+++ src/class-components.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+export default class Calentdar extends Component{
+    getDate(){
+        const dayNames=['Sunday','Monday','Tuesday','Wednesday'
+            ,'Thursday','Friday','Saturday']
+        const monthNames=
+
+    }
+
+    render(){
+        return <div>{this.getDate()}</div>>
+    }
+}
+
