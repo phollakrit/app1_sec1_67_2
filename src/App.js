976eb1a36918d42370f67208d2318012ee816200
pat123 <phollakrit@hotmail.com>
2025-01-16 09:45:33 +0700

    Add class-components.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
+import Calendar from "./class-components";
 
 function App() {
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
--- src/class-components.js
+++ src/class-components.js
@@ -1,15 +1,22 @@
 import React,{Component} from "react";
 
-export default class Calentdar extends Component{
+export default class Calendar extends Component{
     getDate(){
         const dayNames=['Sunday','Monday','Tuesday','Wednesday'
             ,'Thursday','Friday','Saturday']
-        const monthNames=
-
+        const monthNames=['January','February','March','April'
+            ,'May','June','July','August','September','October'
+            ,'November','December']
+        const date=new Date()
+        const weekDay=dayNames[date.getDay()]
+        const day=date.getDate()
+        const month=monthNames[date.getMonth()]
+        const year=date.getFullYear()
+        return `${weekDay} ${day} ${month} ${year}`
     }
 
     render(){
-        return <div>{this.getDate()}</div>>
+        return <div>{this.getDate()}</div>
     }
 }
 
