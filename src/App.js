4b89f66e0b91be87bb97b199a3c04c1cf8f02ff4
phollakrit <66252619+phollakrit@users.noreply.github.com>
2025-02-20 11:48:51 +0700

    Add form-select-file.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Router3 from './router3';
+import FormSelectFile from './form-select-file';
 
 export default function App() {  
-  return <Router3/>
+  return <FormSelectFile/>
 }
 
